a6b56882054df8361f8100ac52955d00e28457d0
shubhajeet <shubha.pradhan12@gmail.com>
2022-01-14 02:19:39 +0530

    styling enhanced & comment order updated

--- myblog/static/blog/js/script.js
+++ myblog/static/blog/js/script.js
@@ -38,3 +38,8 @@ function setTheme(mode){
 
 	localStorage.setItem('theme', mode)
 }
+
+var script = document.createElement('script');
+script.type = 'text/javascript'
+script.src = "https://cdn.tiny.cloud/1/1054g45up3jjvf5rd2kgaud1jecjjcx35yz8s8aw13mzhdgl/tinymce/5/tinymce.min.js";
+    document.head.appendChild(script);
