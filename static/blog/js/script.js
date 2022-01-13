8eb8ec4e793dd774931e72f97a13bcfed871a00d
shubhajeet <shubha.pradhan12@gmail.com>
2022-01-14 02:30:41 +0530

    minor changes

--- static/blog/js/script.js
+++
@@ -1,4 +0,0 @@
-var script = document.createElement('script');
-script.type = 'text/javascript'
-script.src = "https://cdn.tiny.cloud/1/1054g45up3jjvf5rd2kgaud1jecjjcx35yz8s8aw13mzhdgl/tinymce/5/tinymce.min.js";
-document.head.appendChild(script);
