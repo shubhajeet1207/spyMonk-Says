bd4e96dec81167344f54bf9e28a10ba38a054468
shubhajeet <shubha.pradhan12@gmail.com>
2022-01-15 00:47:49 +0530

    (tiny-mce): body editor features removed

--- myblog/static/blog/js/script.js
+++ myblog/static/blog/js/script.js
@@ -39,7 +39,7 @@ function setTheme(mode){
 	localStorage.setItem('theme', mode)
 }
 
-var script = document.createElement('script');
-script.type = 'text/javascript'
-script.src = "https://cdn.tiny.cloud/1/1054g45up3jjvf5rd2kgaud1jecjjcx35yz8s8aw13mzhdgl/tinymce/5/tinymce.min.js";
-    document.head.appendChild(script);
+//var script = document.createElement('script');
+//script.type = 'text/javascript'
+//script.src = "https://cdn.tiny.cloud/1/1054g45up3jjvf5rd2kgaud1jecjjcx35yz8s8aw13mzhdgl/tinymce/5/tinymce.min.js";
+//    document.head.appendChild(script);
