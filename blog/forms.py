684ba21d399bc41411014966ae4b403d46c3643c
shubhajeet <shubha.pradhan12@gmail.com>
2022-01-14 22:22:50 +0530

    sharing form updated

--- blog/forms.py
+++ blog/forms.py
@@ -2,12 +2,12 @@ from django import forms
 from .models import Comment
 
 
+
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
-
 
 class CommentForm(forms.ModelForm):
     class Meta:
