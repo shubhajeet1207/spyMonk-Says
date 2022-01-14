85c8b6fcb53a787eadcd2f850bc3bb0b6bd782c1
shubhajeet <shubha.pradhan12@gmail.com>
2022-01-14 22:24:17 +0530

    migrations done(added draft/publish section)

--- blog/models.py
+++ blog/models.py
@@ -6,11 +6,11 @@ from ckeditor.fields import RichTextField
 from taggit.managers import TaggableManager
 
 
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager, self).get_queryset()\
             .filter(status='published')
-
 
 class Post(models.Model):
     STATUS_CHOICES = (
