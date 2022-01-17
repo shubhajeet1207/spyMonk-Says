740fb5c96c2d1df06d06e476483770b8fa4173e7
shubhajeet <shubha.pradhan12@gmail.com>
2022-01-17 20:27:48 +0530

    commit v4

--- myblog/settings.py
+++ myblog/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'be9(c$x@g@d!x+bjcjtv!+bl7(pfec=)$uzrxbj867dfe4rp#7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
